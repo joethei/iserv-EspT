25fb58b8a46f779caaa0afc8d44d650f4aef2e27
Johannes Theiner <j.theiner@live.de>
2021-05-18 08:46:02 +0200

    - register menu listener as service - create first entity

---
+++ modules/DigiHelfer/EspT/Entity/CreationSettings.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace DigiHelfer\EspT\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use IServ\CrudBundle\Entity\CrudInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class CreationSettings
+ * @package DigiHelfer\EspT\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="espt_settings")
+ * @ORM\HasLifecycleCallbacks
+ */
+class CreationSettings implements CrudInterface{
+
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="start_date", type="datetime")
+     * @var DateTime
+     * @Assert\NotBlank()
+     */
+    private $startDate;
+
+    /**
+     * @ORM\Column(name="end_date", type="datetime")
+     * @var DateTime
+     * @Assert\NotBlank()
+     */
+    private $endDate;
+
+    /**
+     * @ORM\Column(name="registration_start", type="datetime")
+     * @var DateTime
+     * @Assert\NotBlank()
+     */
+    private $registrationStart;
+
+    /**
+     * @ORM\Column(name="registration_end", type="datetime")
+     * @var DateTime
+     * @Assert\NotBlank()
+     */
+    private $registrationEnd;
+
+    /**
+     * @ORM\Column(name="normal_length", type="integer")
+     * @var int
+     * @Assert\Positive()
+     */
+    private $normalLength;
+
+    /**
+     * @ORM\Column(name="invite_length", type="integer")
+     * @var integer
+     * @Assert\Positive()
+     */
+    private $inviteLength;
+
+    /**
+     * @ORM\Column(name="max_number_of_invites", type="integer")
+     * @var integer
+     * @Assert\Positive()
+     */
+    private $maxNumberOfInvites;
+
+    /**
+     * @ORM\Column(name="event_type", type="integer")
+     * @var EventType
+     * @Assert\NotBlank()
+     */
+    private $eventType;
+
+    public function __toString() {
+        return "";
+    }
+
+    public function getId() : ?int {
+        return $this->id;
+    }
+}
---
+++ modules/DigiHelfer/EspT/Entity/CreationSettingsType.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DigiHelfer\EspT\Entity;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class CreationSettingsType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('');
+    }
+}
