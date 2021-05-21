163ea1e46a32bcd1fd027695478000a85c71b4cc
Johannes Theiner <j.theiner@live.de>
2021-05-21 10:43:29 +0200

    translating the first form

--- modules/DigiHelfer/EspTBundle/Entity/CreationSettingsType.php
+++ modules/DigiHelfer/EspTBundle/Entity/CreationSettingsType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateIntervalType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,14 +16,49 @@ class CreationSettingsType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
-            ->add('date', DateType::class)
-            ->add('start', TimeType::class)
-            ->add('end', TimeType::class)
-            ->add('regStart', DateTimeType::class)
-            ->add('regEnd', DateTimeType::class)
-            ->add('normalLength', DateIntervalType::class)
-            ->add('inviteLength', DateIntervalType::class)
-            ->add('maxNumberOfInvites', IntegerType::class);
+            ->add('date', DateType::class, [
+                'label' => _("Date")
+            ])
+            ->add('start', TimeType::class, [
+                'label' => _('StartTime')
+            ])
+            ->add('end', TimeType::class, [
+                'label' => _("EndTime")
+            ])
+            ->add('regStart', DateTimeType::class, [
+                'label' => _("RegStart")
+            ])
+            ->add('regEnd', DateTimeType::class, [
+                'label' => _("RegEnd")
+            ])
+            ->add('normalLength', DateIntervalType::class, [
+                'label' => _("NormalLength"),
+                'with_years' => false,
+                'with_months' => false,
+                'with_days' => false,
+                'with_hours' => false,
+                'with_minutes' => true,
+                'labels' => [
+                    'minutes' => _("minutes")
+                ]
+            ])
+            ->add('inviteLength', DateIntervalType::class, [
+                'label' => _("InviteLength"),
+                'with_years' => false,
+                'with_months' => false,
+                'with_days' => false,
+                'with_hours' => false,
+                'with_minutes' => true,
+                'labels' => [
+                    'minutes' => _("minutes")
+                ]
+            ])
+            ->add('maxNumberOfInvites', IntegerType::class, [
+                'label' => _("MaxNumberOfInvites")
+            ])
+        ->add('save', SubmitType::class, [
+            'label' => _("save")
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void {
