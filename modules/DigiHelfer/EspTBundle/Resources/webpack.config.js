21c28e483dbb71c0dc90a10648ac7461693f9cc8
Johannes Theiner <j.theiner@live.de>
2021-06-04 09:36:28 +0200

    fix webpack config

--- modules/DigiHelfer/EspTBundle/Resources/webpack.config.js
+++ modules/DigiHelfer/EspTBundle/Resources/webpack.config.js
@@ -7,8 +7,8 @@ let webpackConfig = {
         'css/espt': './assets/less/espt.less',
         'css/schedule': './assets/less/schedule.less',
         'js/espt': './assets/js/espt.js',
-        'js/scheduler': 'js/vue-scheduler-lite.js',
-        'js/schedule': 'js/schedule.js',
+        'js/scheduler': './assets/js/vue-scheduler-lite.js',
+        'js/schedule': './assets/js/schedule.js',
 
         'img/interview.png': './assets/img/interview.png',
     },
