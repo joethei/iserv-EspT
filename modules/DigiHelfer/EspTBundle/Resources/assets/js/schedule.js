a9fce953aa09db902dc282704d1a84dbd97d8968
Johannes Theiner <j.theiner@live.de>
2021-06-08 11:38:20 +0200

    adding jq.schedule as display lib

--- modules/DigiHelfer/EspTBundle/Resources/assets/js/schedule.js
+++ modules/DigiHelfer/EspTBundle/Resources/assets/js/schedule.js
@@ -1,11 +1,56 @@
 import Schedule from './vue/schedule';
-import vueSchedulerLite from './vue-scheduler-lite';
+import VueSchedulerLite from './vue-scheduler-lite';
 import Vue from 'vue';
 
 new Vue({
     el: '#schedule',
     render: h => h(Schedule),
     components: {
-        'vue-scheduler-lite': vueSchedulerLite
+        VueSchedulerLite
     }
+});
+
+$(function() {
+   var $sc = jQuery("#schedule").timeSchedule({
+       startTime: "07:00", // schedule start time(HH:ii)
+       endTime: "21:00",   // schedule end time(HH:ii)
+       widthTime: 60 * 10,  // cell timestamp example 10 minutes
+       timeLineY: 60,       // height(px)
+       verticalScrollbar: 20,   // scrollbar (px)
+       timeLineBorder: 2,   // border(top and bottom)
+       bundleMoveWidth: 6,  // width to move all schedules to the right of the clicked time line cell
+       draggable: false,
+       resizable: false,
+       resizableLeft: true,
+       rows : {
+           '0': {
+               title: 'Title Area1',
+               schedule: [
+                   {
+                       start: '09:00',
+                       end: '12:00',
+                       text: 'Text Area',
+                       data: {}
+                   },
+                   {
+                       start: '11:00',
+                       end: '14:00',
+                       text: 'Text Area',
+                       data: {}
+                   }
+               ]
+           },
+           '1': {
+               title: 'Title Area2',
+               schedule: [
+                   {
+                       start: '16:00',
+                       end: '17:00',
+                       text: 'Text Area',
+                       data: {}
+                   }
+               ]
+           }
+       }
+   });
 });
