1660c026163e0360b9a3c4d7f6d94940c97b9515
Johannes Theiner <j.theiner@live.de>
2021-06-11 10:13:41 +0200

    fixing webpack paths removing dependencies

--- modules/DigiHelfer/EspTBundle/Resources/webpack.config.js
+++ modules/DigiHelfer/EspTBundle/Resources/webpack.config.js
@@ -6,14 +6,7 @@ let webpackConfig = {
     entry: {
         'css/espt': './assets/less/espt.less',
         'css/schedule': './assets/less/schedule.less',
-        'css/jquery-ui': './assets/css/jquery-ui.css',
-        'css/jquery-ui.structure': './assets/css/jquery-ui.structure.css',
-        'css/jquery-ui.theme': './assets/css/jquery-ui.theme.css',
-        'css/jq.schedule': './assets/css/jq-schedule.css',
         'js/espt': './assets/js/espt.js',
-        'js/scheduler': './assets/js/vue-scheduler-lite.js',
-        'js/jquery-ui': './assets/js/jquery-ui.js',
-        'js/jq.schedule': './assets/js/jq.schedule.js',
         'js/schedule': './assets/js/schedule.js',
 
         'img/interview.svg': './assets/img/interview.svg'
