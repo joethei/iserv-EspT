8ea1480366a203ec26decb895d1dcfb40cd4b4ba
Johannes Theiner <kontakt@joethei.xyz>
2021-10-07 12:53:19 +0200

    removing confirmation button on creation again

--- modules/DigiHelfer/EspTBundle/Resources/assets/js/espt.js
+++ modules/DigiHelfer/EspTBundle/Resources/assets/js/espt.js
@@ -3,7 +3,6 @@
 import Schedule from './Schedule.vue';
 import TimeslotTable from "./TimeslotTable";
 import Vue from 'vue';
-import Confirm from 'IServ.Confirm';
 
 
 if($('#espt-table').length) {
@@ -17,27 +16,5 @@ if($('#espt-schedule').length) {
     new Vue({
         el: '#espt-schedule',
         render: h => h(Schedule)
-    });
-}
-if($('#creation_form').length) {
-    $('#creation_form').on('submit', function(e) {
-        e.preventDefault();
-        Confirm.confirm('confirm_creation', {
-            title: _('espt_confirm'),
-            content: _('espt_confirm_creation'),
-            buttons: {
-                confirmButton: {
-                    text: _('OK'),
-                    btnClass: 'btn-primary',
-                    action: function () {
-                        $('#creation_form').submit();}
-                }
-                },
-                cancelButton: {
-                    text: _('Cancel'),
-                    btnClass: 'btn-default',
-                    action: function () {}
-                }
-            });
     });
 }
